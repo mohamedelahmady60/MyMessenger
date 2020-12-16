f3c6ef9960ab08706e736b0b24be22cd0e414efd
Mohamed Elamady <mohamed.elahmady60@gmail.com>
2020-12-16 17:00:28 +0200

    Getting started

---
+++ MyMessanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMessanger
+//
+//  Created by Mo Elahmady on 16/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

9a5dd7af9ca4b607659998b504b8d89234d7dda0
Mohamed Elamady <mohamed.elahmady60@gmail.com>
2020-12-16 17:29:11 +0200

    wrap up getting started

--- MyMessanger/Controllers/ConversationsViewController.swift
+++ MyMessanger/Controllers/ConversationsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ConversationsViewController.swift
 //  MyMessanger
 //
 //  Created by Mo Elahmady on 16/12/2020.
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
 
 }
-
